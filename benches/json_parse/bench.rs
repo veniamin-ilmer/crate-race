a913288431294603d8558b2c1196b83b3fc1aa2a
Veniamin Ilmer <39828997+veniamin-ilmer@users.noreply.github.com>
2018-12-11 01:13:50 +0000

    Update bench.rs

--- benches/json_parse/bench.rs
+++ benches/json_parse/bench.rs
@@ -1,3 +1,7 @@
+///Baseline: Simple json with just one entry.
+///Serial: Opposite of nested. 100 entries listed in a "flat" format, one by one, without any tree structure.
+///Nested: 100 nested entries, each inside of the other.
+
 #[macro_use]
 extern crate bencher;
 
