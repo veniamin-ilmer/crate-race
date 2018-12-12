a81b896d22c08a6e16da248964877fcf8a9663f0
Veniamin Ilmer <39828997+veniamin-ilmer@users.noreply.github.com>
2018-12-12 09:14:32 -0600

    Update bench.rs

--- benches/json_build/bench.rs
+++ benches/json_build/bench.rs
@@ -13,10 +13,10 @@ static NESTED: &str = r#"{"test1":{"test2":{"test3":{"test4":{"test5":{"test6":{
 
 mod _json;
 mod _serde_json;
-mod _json5;
+mod _json_in_type;
 
-benchmark_group!(baseline, _json::baseline, _serde_json::baseline, _json5::baseline);
-benchmark_group!(serial, _json::serial, _serde_json::serial, _json5::serial);
-benchmark_group!(nested, _json::nested, _serde_json::nested, _json5::nested);
+benchmark_group!(baseline, _json::baseline, _serde_json::baseline, _json_in_type::baseline);
+benchmark_group!(serial, _json::serial, _serde_json::serial, _json_in_type::serial);
+benchmark_group!(nested, _json::nested, _serde_json::nested, _json_in_type::nested);
 
 benchmark_main!(baseline, serial, nested);
