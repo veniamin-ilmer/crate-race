63862a02f1b8efa139f4aec6663bdb2d034f2a63
Veniamin Ilmer <39828997+veniamin-ilmer@users.noreply.github.com>
2018-12-13 09:43:25 -0600

    Create _quick_csv.rs

---
+++ benches/csv_parse/_quick_csv.rs
@@ -0,0 +1,31 @@
+use bencher::Bencher;
+
+pub fn baseline(b: &mut Bencher) {
+  b.iter(|| {
+    let mut rdr = quick_csv::Csv::from_reader(super::BASELINE.as_bytes())
+        .has_header(true);
+
+    let row = rdr.next().unwrap().unwrap();
+    assert_eq!(row.next().unwrap(), "Data");
+  });
+}
+
+pub fn rows(b: &mut Bencher) {
+  b.iter(|| {
+    let mut rdr = quick_csv::Csv::from_reader(super::ROWS.as_bytes())
+        .has_header(true);
+
+    let row = rdr..nth(99).unwrap().unwrap();  //99 is the 100th row
+    assert_eq!(row.next().unwrap(), "Row100");
+  });
+}
+
+pub fn headers(b: &mut Bencher) {
+  b.iter(|| {
+    let mut rdr = quick_csv::Csv::from_reader(super::HEADERS.as_bytes())
+        .has_header(true);
+
+    let row = rdr.next().unwrap().unwrap();
+    assert_eq!(row.nth(99).unwrap(), "Data100");  //99 is the 100th column
+  });
+}
