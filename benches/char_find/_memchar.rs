47405f3f687b067a1ef86e0cd0d150161d938b10
Veniamin Ilmer <39828997+veniamin-ilmer@users.noreply.github.com>
2018-12-13 12:32:21 -0600

    Update _memchar.rs

--- benches/char_find/_memchar.rs
+++ benches/char_find/_memchar.rs
@@ -2,18 +2,18 @@ use bencher::Bencher;
 
 pub fn baseline(b: &mut Bencher) {
   b.iter(|| {
-    assert_eq!(memchr::memchr(b'/', super:BASELINE), Some(0));
+    assert_eq!(memchr::memchr(b'/', super::BASELINE), Some(0));
   });
 }
 
 pub fn big_str_first(b: &mut Bencher) {
   b.iter(|| {
-    assert_eq!(memchr::memchr(b'/', super:BIG_STR_FIRST), Some(0));
+    assert_eq!(memchr::memchr(b'/', super::BIG_STR_FIRST), Some(0));
   });
 }
 
 pub fn big_str_last(b: &mut Bencher) {
   b.iter(|| {
-    assert_eq!(memchr::memchr(b'/', super:BIG_STR_LAST), Some(50_000));
+    assert_eq!(memchr::memchr(b'/', super::BIG_STR_LAST), Some(50_000));
   });
 }
