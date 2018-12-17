7b18ffa8415445f67e89de7484fd44be7a9bd43d
Veniamin Ilmer <39828997+veniamin-ilmer@users.noreply.github.com>
2018-12-17 13:04:32 -0600

    Update and rename benches/bigint_multiplication/_num_bigint.rs to benches/bigint_arithmetic/_num_bigint.rs

--- benches/bigint_arithmetic/_num_bigint.rs
+++ benches/bigint_arithmetic/_num_bigint.rs
@@ -7,8 +7,8 @@ pub fn baseline(b: &mut Bencher) {
   b.iter(|| {
     let num_1: BigUint = One::one();
     let mut num_sum: BigUint = One::one();
-    num_sum *= &num_1;
-    assert_eq!(num_sum.to_string(), super::BASELINE);
+    num_sum += &num_1;
+    assert_eq!(BigUint::from(2u8), num_sum);
   });
 }
 
@@ -22,7 +22,13 @@ pub fn fact50(b: &mut Bencher) {
       num_sum += &num_1;
       fact *= &num_sum;
     }
-    assert_eq!(fact.to_string(), super::FACT50);
+    
+    let ten = BigUint::from(10u8);
+    assert_eq!(BigUint::from(0u8), &fact % &ten); //Most right digit
+    for _ in 0..64 {    //Remove all other digits
+      fact /= &ten;
+    }
+    assert_eq!(BigUint::from(3u8), fact); //Most left digit
   });
 }
 
@@ -36,6 +42,12 @@ pub fn fact95(b: &mut Bencher) {
       num_sum += &num_1;
       fact *= &num_sum;
     }
-    assert_eq!(fact.to_string(), super::FACT95);
+    
+    let ten = BigUint::from(10u8);
+    assert_eq!(BigUint::from(0u8), &fact % &ten); //Most right digit
+    for _ in 0..148 {    //Remove all other digits
+      fact /= &ten;
+    }
+    assert_eq!(BigUint::from(1u8), fact); //Most left digit
   });
 }
