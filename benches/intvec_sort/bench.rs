3ce5a56519e699997dd7a40f9d6fdd80f248f9f7
Veniamin Ilmer <39828997+veniamin-ilmer@users.noreply.github.com>
2018-12-18 14:35:01 -0600

    Update bench.rs

--- benches/intvec_sort/bench.rs
+++ benches/intvec_sort/bench.rs
@@ -6,10 +6,17 @@
 //!* *Baseline_big = 10,000 items. Fully sorted already.
 //!* *Random_big = 10,000 items. Completely random.
 //!* *Reverse_big = 10,000 items. Sorted in reverse order.
+//!
+//!"vec" is the out of the box `sort` function that comes with Vec.
 
 #[macro_use]
 extern crate bencher;
 
-benchmark_group!(baseline, _modhere::baseline);
+benchmark_group!(baseline_small, _vec::baseline_small);
+benchmark_group!(random_small, _vec::random_small);
+benchmark_group!(reverse_small, _vec::reverse_small);
+benchmark_group!(baseline_big, _vec::baseline_big);
+benchmark_group!(random_big, _vec::random_big);
+benchmark_group!(reverse_big, _vec::reverse_big);
 
-benchmark_main!(baseline);
+benchmark_main!(baseline_small, random_small, reverse_small, baseline_big, random_big, reverse_big);
